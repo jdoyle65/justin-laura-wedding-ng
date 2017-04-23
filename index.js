9df39d9894f07204b0fc49cc30594b8640a202eb
Justin Doyle <justin.doyle@redspace.com>
2017-04-23 14:11:07 -0300

    remove AWS dynamodb

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
 var express = require('express')
 var app = express();
 var bodyParser  = require('body-parser');
-var AWS = require('aws-sdk')
 app.use(bodyParser.json());
-
-AWS.config.loadFromPath('./config.json');
-var db = new AWS.DynamoDB.DocumentClient();
-
-function findRsvp(rsvpKey) {
-    var params = {
-    TableName : "wedding-rsvps",
-    KeyConditionExpression: 'rsvpKey = :key',
-    ExpressionAttributeValues: { ':key': rsvpKey }
-};
-    db.query(params, (err, data) => {
-        if (err) {
-            console.log('Error');
-            console.log(err);
-        } else {
-            console.log('Query Succeeded');
-            console.log(data);
-        }
-    })
-}
-findRsvp('justindoyle');
 
 const USERS = require('./sample-rsvps.json');
 
